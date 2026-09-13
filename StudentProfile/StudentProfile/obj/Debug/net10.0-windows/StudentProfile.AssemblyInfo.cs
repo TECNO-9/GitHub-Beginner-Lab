20b490864952909cbbe1880608cf6fec7ecdbf6e
Rhyen Miguel Gutierrez Peñamayor <rhyenmiguelg@gmail.com>
2026-09-13 22:03:30 +0800

    Add student contact number.

--- StudentProfile/StudentProfile/obj/Debug/net10.0-windows/StudentProfile.AssemblyInfo.cs
+++ StudentProfile/StudentProfile/obj/Debug/net10.0-windows/StudentProfile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e9d40800ea252d9e3ae07f249712945d7fa475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b81943b1b9505788e2433f8c480f9961efb9176")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
